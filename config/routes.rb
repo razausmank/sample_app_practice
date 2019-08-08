28f04ef539a36e88562c62ab98f35b6e13dc4989
Usman raza <Razausmankhan97@gmail.com>
2019-08-08 21:02:26 +0000

    Add password Reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
 root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -14,4 +18,6 @@ root 'static_pages#home'
   delete '/logout',  to: 'sessions#destroy'
   
   resources :account_activations, only: [:edit]
+  
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -8,9 +8,11 @@ class UserMailerPreview < ActionMailer::Preview
     UserMailer.account_activation(user)
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_rest
-  def password_rest
-    UserMailer.password_rest
+ # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/password_reset
+  def password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
-
 end
