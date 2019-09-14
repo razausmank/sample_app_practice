f0aa6c24a632d53f544e1bca4a99192c0c481a64
Usman raza <Razausmankhan97@gmail.com>
2019-09-14 08:55:19 +0000

    testing api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  namespace :api, defaults: {format: :json} do
+      namespace :v1 do
+        resources :users
+      end
+    end
 
   get 'password_resets/new'
 
